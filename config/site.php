acf7029ad236b86059893c2f9fe9a728fad30a5f
Milton Dey Mishu <114005218+softengmilton@users.noreply.github.com>
2025-07-17 00:42:19 +0600

    MediaMan added

---
+++ config/site.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'allowedMediaType' => ['image', 'video', 'pdf'],
+
+    'allowedFileExtensionToUpload' => [
+        'image' => ['jpeg', 'jpg', 'png', 'svg'],
+        'video' => ['mp4'],
+        'pdf'   => ['pdf'],
+    ],
+
+    'allowedMediaRole' => [
+        'other',
+        'profile_image',
+    ],
+];
