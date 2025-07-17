440523e35237f870e7c6262d860a095c5e957a7f
Milton Dey Mishu <114005218+softengmilton@users.noreply.github.com>
2025-07-17 16:59:15 +0600

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,13 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $this->call([
+            // UserSeeder::class,
         ]);
     }
 }
