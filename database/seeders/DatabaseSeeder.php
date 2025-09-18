a4afd737252674d7b20083c00cc7282fce2724d1
Milton Dey Mishu <114005218+softengmilton@users.noreply.github.com>
2025-09-18 19:18:29 +0600

    updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'admin',
             'email' => 'admin@gmail.com',
-            'password' => bcrypt('password'),
+            'password' => bcrypt('AizRVv7s7Q50'),
         ]);
 
         // $this->call([
--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:trigger-monthly-adjustments')->everyFiveSeconds();
+Schedule::command('app:trigger-monthly-adjustments')->lastDayOfMonth('23:00');
