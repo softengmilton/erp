5ee3de0808dc180356102d2f97092d359156f673
Milton Dey Mishu <114005218+softengmilton@users.noreply.github.com>
2025-12-15 21:50:29 +0600

    Update AppSidebar.vue

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -94,10 +94,10 @@ const mainNavItems: NavItem[] = [
             title: 'Stock Product Reports',
             href: '/store/stock-product-reports',
           },
-          {
-          title: 'Assets',
-          href: '/store/total-asset',
-          },
+        //   {
+        //   title: 'Assets',
+        //   href: '/store/total-asset',
+        //   },
         ],
       },
       {
